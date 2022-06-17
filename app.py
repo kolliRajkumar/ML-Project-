23e83144f838c9c7b2a51deb76ece588efa45e7f
kolliRajkumar <rajkumar.ce@outlook.com>
2022-06-17 22:19:31 +0530

    Docker File Added and Tested Sucessfully

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 
-app =Flask(_name_);
+app=Flask(__name__)
 
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting a new Machine Learning Project"
+    return "Starting Machine Learning Project"
 
 
-
-if _name_ == "_main_":
+if __name__=="__main__":
     app.run(debug=True)
